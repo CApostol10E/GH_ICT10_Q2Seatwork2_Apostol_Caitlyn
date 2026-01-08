9c32043874c2218eb08cb9971b7b2977a908fcdb
Breeeee! <brokenbunnyytroblox6943@gmail.com>
2026-01-08 08:56:30 +0800

    Refactor main.py for improved readability

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 first_name = input("Enter First Name: ")
 last_name = input("Enter Last Name: ")
 
-# Step 2: Define subjects and units
 subjects = ["Science", "Mathematics", "English", "History", "PE"]
 units = (3, 3, 3, 2, 1)  # Tuple for the number of units per subject
 
-# Step 3: Get Grades from the user
 grades = []
 for subject in subjects:
     grade = float(input(f"Enter your grade in {subject}: "))
     grades.append(grade)
 
-# Step 4: Calculate General Weighted Average (GWA)
 total_units = sum(units)
 weighted_sum = sum(g * u for g, u in zip(grades, units))
 gwa = round(weighted_sum / total_units, 2)
 
-# Step 5: Display Results
 print("\n===== Student Information =====")
 print(f"Name: {first_name} {last_name}")
 
@@ -24,5 +20,16 @@ print("\n===== Summary of Grades =====")
 for subj, grade in zip(subjects, grades):
     print(f"{subj}: {grade}")
 
+def pos_or_neg(e):
+    document.getElementById('output1').innerHTML = ''
+    num1 = document.getElementById('num1').value
+
+
+    if num1 > 0:
+        display(f'Number {num1} is positive', target='output1')
+    else:
+        display(f'Number {num1} is negative', target='output1')
+
 print("\n===== General Weighted Average =====")
+
 print(f"GWA: {gwa}")
